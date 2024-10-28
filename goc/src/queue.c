4f47677282533502ad4b0c68bbd1e610b0a6fd13
mimin514 <my.nguyen2004@hcmut.edu.vn>
2024-10-28 13:43:24 +0700

    ok

--- goc/src/queue.c
+++ goc/src/queue.c
@@ -9,12 +9,25 @@ int empty(struct queue_t * q) {
 
 void enqueue(struct queue_t * q, struct pcb_t * proc) {
         /* TODO: put a new process to queue [q] */
+        if (q->size < MAX_QUEUE_SIZE) {  // Ensure queue is not full
+        q->proc[q->size] = proc;      // Add the process to the end of the queue
+        q->size++;                    // Increase the size of the queue
+    }
 }
 
 struct pcb_t * dequeue(struct queue_t * q) {
         /* TODO: return a pcb whose prioprity is the highest
          * in the queue [q] and remember to remove it from q
          * */
-	return NULL;
+	//return NULL;
+        if (q->size == 0) return NULL;
+
+    struct pcb_t *proc = q->proc[0];
+    for (int i = 1; i < q->size; i++) {
+        q->proc[i - 1] = q->proc[i];
+    }
+    q->size--;
+    return proc;                   // Increase the size of the queue
+    
 }
 
